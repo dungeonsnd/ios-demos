5d776f22186619bab23927dfe829227fb803f139
jeffery <jeffery@gmail.com>
2015-09-23 06:50:14 +0800

    add readme.txt

---
+++ port_libevent/demo/demo_port_libevent/demo_port_libevent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo_port_libevent
+//
+//  Created by jeffery on 15/8/25.
+//  Copyright © 2015年 jeffery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ port_libevent/demo/demo_port_libevent/demo_port_libevent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo_port_libevent
+//
+//  Created by jeffery on 15/8/25.
+//  Copyright © 2015年 jeffery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
